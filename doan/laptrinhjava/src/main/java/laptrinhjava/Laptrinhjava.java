2641f23430f88ed7378838ad81427a36d2c6975b
tongthanhdat009 <84076965+tongthanhdat009@users.noreply.github.com>
2024-03-12 13:46:54 +0700

    Add files via upload

---
+++ doan/laptrinhjava/src/main/java/laptrinhjava/Laptrinhjava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package laptrinhjava;
+
+/**
+ *
+ * @author dat
+ */
+public class Laptrinhjava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

18f036bca5b14e1fc049cf2bbb31f35f037fb9db
AangTN <nguyenminhtuan06012004@gmail.com>
2024-03-14 00:55:08 +0700

    chua test

---
+++ DSHoaDon.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class DSHoaDon {
+    private ArrayList<HoaDon> dsHoaDon;
+    public DSHoaDon()
+    {
+        dsHoaDon = new ArrayList<>();
+    }
+    public DSHoaDon(ArrayList<HoaDon> dsHoaDon)
+    {
+        this.dsHoaDon = dsHoaDon;
+    }
+    public void them(String maHoiVien,String maCoSo,int tongTien, LocalDate ngayXuatHoaDon)
+    {
+        HoaDon newHoaDon = new HoaDon(maHoiVien,maCoSo,tongTien,ngayXuatHoaDon);
+        dsHoaDon.add(newHoaDon);
+    }
+    public void xoa(String maHoaDon)
+    {
+        for(HoaDon i:dsHoaDon)
+        if(i.getMaHoaDon().equals(maHoaDon)) dsHoaDon.remove(i);
+    }
+    public void sua(String maHoaDon,String maHoiVien,String maCoSo,int tongTien, LocalDate ngayXuatHoaDon)
+    {
+        for(HoaDon i:dsHoaDon)
+        if(i.getMaHoaDon().equals(maHoaDon))
+        {
+            i.setMaHoiVien(maHoiVien);
+            i.setMaCoSo(maCoSo);
+            i.setTongTien(tongTien);
+            i.setNgayXuatHoaDon(ngayXuatHoaDon);
+        }
+    }
+}

90163ef97c0f5b3a080d14caab7ac594fd925d08
tongthanhdat009 <gamingthanhdat@gmail.com>
2024-03-19 15:10:59 +0700

    GUILogin

--- doan/src/laptrinhjava/src/GUILogin.java
+++ doan/src/laptrinhjava/src/GUILogin.java
@@ -6,29 +6,43 @@ public class GUILogin extends JFrame implements ActionListener{
     // private dsHoiVien dsHoiVien1 = new dsHoiVien();
     private JTextField tenTK = new JTextField();
     private JPasswordField mk = new JPasswordField();
-    private ImageIcon icon = new ImageIcon("giphy.gif");
+    //CHỈNH ẢNH
+    ImageIcon icon = new ImageIcon("./asset/gif/login.gif");
+    Image scaleImage = icon.getImage().getScaledInstance(775/2 + 5, 265,Image.SCALE_DEFAULT);
     public GUILogin()
     {
         // docDuLieu();
-        setBounds(500,500,775,250);
-        setLayout(new GridLayout(1,1,5,0));
+        setSize(new Dimension (775,300));
+        setLocationRelativeTo(null);
+        setLayout(new GridLayout(1,2,5,0));
 
         JPanel TTDN = new JPanel();
         TTDN.setLayout(new BorderLayout());
+        
         JPanel header = new JPanel();
         header.setLayout(new FlowLayout());
+
+        
         JLabel tieuDe = new JLabel("Đăng nhập");
         tieuDe.setFont(new Font("Arial",Font.BOLD,30));
         header.add(tieuDe);
         TTDN.add(header, BorderLayout.NORTH);
 
         JPanel TTDNMain = new JPanel();
-        TTDNMain.setLayout(new GridLayout(2,2,0,10));
-        tenTK.setPreferredSize(new Dimension(200,50));
+        TTDNMain.setLayout(null);
+
+        tenTK.setPreferredSize(new Dimension(175,30));
+
         JLabel tenDangNhap = new JLabel("Tên đăng nhập:");
         tenDangNhap.setFont(new Font("Arial",Font.ITALIC,20));
+        tenDangNhap.setBounds(5,10,150,30);
+        tenTK.setBounds(160,10,175,30);
+
         JLabel matKhau = new JLabel("Mật khẩu:");
         matKhau.setFont(new Font("Arial",Font.ITALIC,20));
+        matKhau.setBounds(5,75,150,30);
+        mk.setBounds(160,75,175,30);
+        
         TTDNMain.add(tenDangNhap);
         TTDNMain.add(tenTK);
         TTDNMain.add(matKhau);
@@ -36,17 +50,22 @@ public class GUILogin extends JFrame implements ActionListener{
         TTDN.add(TTDNMain,BorderLayout.CENTER);
 
         JButton dangNhap = new JButton("Dang Nhap");
-        JButton dangKy = new JButton("Dang ky");
+        dangNhap.setPreferredSize(new Dimension(100,30));
         dangNhap.addActionListener(this);
+        
+        JButton dangKy = new JButton("Dang ky");
+        dangKy.setPreferredSize(new Dimension(100,30));
+
         JPanel TTDNSub = new JPanel();
         TTDNSub.setLayout(new FlowLayout());
         TTDNSub.add(dangKy);
         TTDNSub.add(dangNhap);
         TTDN.add(TTDNSub,BorderLayout.SOUTH);
 
+        // JPanel anhPanel = new JPanel();
         JLabel anh = new JLabel();
-        anh.setIcon(icon);
-
+        anh.setIcon(new ImageIcon(scaleImage));
+        // anhPanel.add(anh);
         add(TTDN);
         add(anh);
 

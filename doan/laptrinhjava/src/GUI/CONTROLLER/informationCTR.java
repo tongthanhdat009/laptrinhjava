a43224806c68fcf00d8b4649b28ecca62fcf4ce1
tongthanhdat009 <gamingthanhdat@gmail.com>
2024-09-17 23:56:35 +0700

    xem thông tin cá nhân

---
+++ doan/laptrinhjava/src/GUI/CONTROLLER/informationCTR.java
@@ -0,0 +1,13 @@
+package GUI.CONTROLLER;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class informationCTR extends JPanel{
+	private static final long serialVersionUID = 7765958644533896434L;
+
+	public informationCTR() {
+		this.setSize(new Dimension(1200,900));
+	}
+}
